63f6a6657921828352aebb4f4d48cbf81c2009e8
Franck <136079691+Freddyede@users.noreply.github.com>
2024-04-23 11:47:22 +0200

    Finish flat

--- src/FlatArray.js
+++ src/FlatArray.js
@@ -1,8 +1,35 @@
 export class FlatArray {
     constructor(tab) {
-        this.tab = tab !== undefined ? tab : [];
+        this.value = [];
+        this.tab = tab;
     }
+    verifyIsArray = array => Array.isArray(array);
+    recursiveArray = val => {
+        if(this.verifyIsArray(val)) {
+            this.runArray(val);
+        }
+        else {
+            if(this.verifyIsArray(val)){
+                this.runArray(val);
+            }
+            else {
+                this.value[this.value.length] = val;
+            }
+        }
+
+    }
+    runArray = array => {
+        for (let i = 0; i < array.length; i++) {
+            this.recursiveArray(array[i]);
+        }
+    }
+
     flat() {
-        console.log(this.tab.length %2 === 0);
+        for (let i = 0; i < this.tab.length; i++) {
+            if(Array.isArray(this.tab[i])) {
+                this.runArray(this.tab[i]);
+            }
+        }
+        return this.value;
     }
 }
